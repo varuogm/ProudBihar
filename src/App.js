552d142968b9f1282aa651b6907ae869604d07d2
gourav.majee <gourav.majee@intimetec.com>
2023-05-26 16:46:29 +0530

    Added Bihar 3d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { Suspense, useState } from "react";
+import { motion, MotionConfig, useMotionValue } from "framer-motion";
+import { Shapes } from "./Shapes";
+import { transition } from "./Settings";
+import useMeasure from "react-use-measure";
 
-function App() {
+export default function App() {
+  const [ref, bounds] = useMeasure({ scroll: false });
+  const [isHover, setIsHover] = useState(false);
+  const [isPress, setIsPress] = useState(false);
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const resetMousePosition = () => {
+    mouseX.set(0);
+    mouseY.set(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MotionConfig transition={transition}>
+      <motion.button
+        ref={ref}
+        initial={false}
+        animate={isHover ? "hover" : "rest"}
+        whileTap="press"
+        variants={{
+          rest: { scale: 1 },
+          hover: { scale: 1.5 },
+          press: { scale: 1.4 }
+        }}
+        onHoverStart={() => {
+          resetMousePosition();
+          setIsHover(true);
+        }}
+        onHoverEnd={() => {
+          resetMousePosition();
+          setIsHover(false);
+        }}
+        onTapStart={() => setIsPress(true)}
+        onTap={() => setIsPress(false)}
+        onTapCancel={() => setIsPress(false)}
+        onPointerMove={(e) => {
+          mouseX.set(e.clientX - bounds.x - bounds.width / 2);
+          mouseY.set(e.clientY - bounds.y - bounds.height / 2);
+        }}
+      >
+        <motion.div
+          className="shapes"
+          variants={{
+            rest: { opacity: 0 },
+            hover: { opacity: 1 }
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="pink blush" />
+          <div className="blue blush" />
+          <div className="container">
+            <Suspense fallback={null}>
+              <Shapes
+                isHover={isHover}
+                isPress={isPress}
+                mouseX={mouseX}
+                mouseY={mouseY}
+              />
+            </Suspense>
+          </div>
+        </motion.div>
+        <motion.div
+          variants={{ hover: { scale: 0.85 }, press: { scale: 1.1 } }}
+          className="label"
+        >
+        🏳️‍🌈-har
+        </motion.div>
+      </motion.button>
+    </MotionConfig>
   );
 }
-
-export default App;
---
+++ src/use-smooth-transform.js
@@ -0,0 +1,6 @@
+
+import { useSpring, useTransform } from "framer-motion";
+
+export function useSmoothTransform(value, springOptions, transformer) {
+  return useSpring(useTransform(value, transformer), springOptions);
+}
